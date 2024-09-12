18cece7e8e14a36ffe0bb6b62bf548d910333fac
Monatric <nawpnawp8@gmail.com>
2024-09-13 01:51:28 +0800

    Add testing

--- lib/main.rb
+++ lib/main.rb
@@ -1,34 +1,26 @@
 require_relative "node"
 require_relative "tree"
 
-arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+arr = (Array.new(15) { rand(1..100) })
+p arr
 test = Tree.new(arr)
-test.insert(0)
-test.insert(6)
-test.insert(8)
-test.insert(10)
-test.insert(11)
-test.insert(12)
-
-test.insert(13)
-test.insert(14)
-
-# test.delete(8)
-# p test.root.right
-# test.level_order { |e| puts "test #{e}" }
-# p test.level_order
-p test.preorder
-# test.preorder { |e| puts "test #{e}" }
-p test.inorder
-# test.inorder { |e| puts "test #{e}" }
-p test.postorder
-# test.postorder { |e| puts "test #{e}" }
-# p test.find(4)
-# p test.height(9)
 test.pretty_print
+puts "Is it balanced?: #{test.balanced?}"
+puts "Level order: #{test.level_order}"
+puts "Preorder: #{test.preorder}"
+puts "Postorder: #{test.postorder}"
+puts "Inorder: #{test.inorder}"
+puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+10.times do |n|
+  test.insert(rand(100..200))
+  if n == 9
+    test.pretty_print
+    puts "Is it balanced?: #{test.balanced?}"
+  end
+end
 
-# p test.depth(5)
-p test.balanced?
-p test.inorder
-p test.rebalance
-p test.balanced?
+test.rebalance
+puts "Level order: #{test.level_order}"
+puts "Preorder: #{test.preorder}"
+puts "Postorder: #{test.postorder}"
+puts "Inorder: #{test.inorder}"
